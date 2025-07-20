ad598e39f16c954a9ef118c559813c6d5144b9fd
TomaTeQuila <amaroeduard@gmail.com>
2025-07-19 20:02:52 -0400

    feat: categories sidebar for filter offenses, and some fixes

---
+++ src/data/officer.ts
@@ -0,0 +1,17 @@
+export interface SourceOfficer {
+    name: string;
+    surname: string;
+    gender: string;
+    badge: string;
+    rank: string;
+    serviceHours: number;
+}
+
+export const sourceOfficerData = {
+    name: "Pepito",
+    surname: "Perez",
+    gender: "m",
+    badge: "PD-1234",
+    rank: "Sargento",
+    serviceHours: 65,
+}
